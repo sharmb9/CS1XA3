ddfa7c5fa90bd7dc38b90e0cee6d883a3851d79c
Sharma Bilaval <sharmb9@mcmaster.ca>
2018-02-22 02:41:47 -0500

    Added third feature in ProjectAnalyze.sh

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 #Checking the status of the repo
+
 git status
 
 #Puts all uncommited changes in a file changes.log
+
 git diff > changes.log
 
+#Puts each line from every file of your project with tag #TODO into a file todo.log
+
+grep -r --exclude="todo.log" "#TODO" * > todo.log
 
