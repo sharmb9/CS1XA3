3b6e3c3ae4813b3302584e44ae03f763ea27aa99
Sharma Bilaval <sharmb9@mcmaster.ca>
2018-02-25 18:33:28 -0500

    Added a feature

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -13,8 +13,36 @@ git diff >> changes.log
 
 #Puts each line from every file of your project with tag #TODO into a file todo.log
 
-grep -r --exclude="todo.log" --exclude="ProjectAnalyze.sh" "#TODO" * >> todo.log
+grep -r --exclude={todo.log,ProjectAnalyze.sh,changes.log,error.log} "#TODO" * >> todo.log
 
 #Checks all haskell files for syntax errors and puts the result into error.log
 find . -name "*.hs" -exec ghc -fno-code {} \; >> error.log
+
+#Ask user if they want to view any log file
+echo "Your changes, haskell errors and TODO tags have been logged into log files, do you wish to view them?"
+read input 
+if ["$input" == "Y"]; then 
+	echo "Type C if you wish to view changes.log, type E if you wish to view errors.log and type T to view TODO.log. Type A if you wish to view your log files at once"
+	read type
+	if ["$type" == "C"]; then
+		cat changes.log
+	elif ["$type" == "E"]; then
+		cat errors.log
+	elif ["$type" == "T"]; then
+		cat todo.log
+	elif ["$type" == "A"]; then
+		echo "Processing changes.log..."
+		cat changes.log
+		echo "Processing todo.log..."
+		cat todo.log
+		echo "Processing errors.log..."
+		cat errors.log
+	else
+		echo "Oops seems like you enterd the wrong key"
+		#something like break
+	fi
+elif ["$input" == "N"]; then
+echo "Have a nice day"
+#enter else statemnt for wronf key
+fi
 
