8a95a4bd4190b3f8e1157ef7d52dc69d42482faa
Sharma Bilaval <sharmb9@mcmaster.ca>
2018-02-25 15:36:39 -0500

    Addded assignmnet specifies features

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-#Checking the status of the repo
+#Checking if local repo is up to date with remote repo
 
+#updates your local repo on the machine
+git fetch
+#Checks if local repo is up to date with remote 
 git status
 
 #Puts all uncommited changes in a file changes.log
 
-git diff > changes.log
+git diff >> changes.log
 
 #Puts each line from every file of your project with tag #TODO into a file todo.log
 
-grep -r --exclude="todo.log" "#TODO" * > todo.log
+grep -r --exclude="todo.log" --exclude="ProjectAnalyze.sh" "#TODO" * >> todo.log
 
 #Checks all haskell files for syntax errors and puts the result into error.log
-find . -name "*.hs" -exec ghc -fno-code {} \; > error.log
+find . -name "*.hs" -exec ghc -fno-code {} \; >> error.log
 
