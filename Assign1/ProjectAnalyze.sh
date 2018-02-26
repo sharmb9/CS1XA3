4aa78f4a4091edf713369dd5ebdf2a1d3c04f247
Sharma Bilaval <sharmb9@mcmaster.ca>
2018-02-25 19:08:02 -0500

    Added feature

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -4,7 +4,7 @@
 
 #updates your local repo on the machine
 git fetch
-#Checks if local repo is up to date with remote 
+#Checks if local repo is up to date with remote
 git status
 
 #Puts all uncommited changes in a file changes.log
@@ -20,17 +20,17 @@ find . -name "*.hs" -exec ghc -fno-code {} \; >> error.log
 
 #Ask user if they want to view any log file
 echo "Your changes, haskell errors and TODO tags have been logged into log files, do you wish to view them?"
-read input 
-if ["$input" == "Y"]; then 
+read input
+if [ $input == "Y" ]; then
 	echo "Type C if you wish to view changes.log, type E if you wish to view errors.log and type T to view TODO.log. Type A if you wish to view your log files at once"
 	read type
-	if ["$type" == "C"]; then
+	if [ $type == "C" ]; then
 		cat changes.log
-	elif ["$type" == "E"]; then
+	elif [ $type == "E" ]; then
 		cat errors.log
-	elif ["$type" == "T"]; then
+	elif [ $type == "T" ]; then
 		cat todo.log
-	elif ["$type" == "A"]; then
+	elif [ $type == "A" ]; then
 		echo "Processing changes.log..."
 		cat changes.log
 		echo "Processing todo.log..."
@@ -41,7 +41,7 @@ if ["$input" == "Y"]; then
 		echo "Oops seems like you enterd the wrong key"
 		#something like break
 	fi
-elif ["$input" == "N"]; then
+elif [ $input == "N" ]; then
 echo "Have a nice day"
 #enter else statemnt for wronf key
 fi
