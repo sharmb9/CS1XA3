2da3d186b7a4f05d15c88aeaca56a2f5cb6b99ce
Sharma Bilaval <sharmb9@mcmaster.ca>
2018-03-15 15:37:33 -0400

    Added files for website

---
+++ Assign2/js/ticTactoe.py
@@ -0,0 +1,10 @@
+the_board= {1: " ", 2: " ", 3: " ",
+           4: " ", 5: " ", 6: " ",
+           7: " ", 8: " ", 9: " "}
+
+def print_board():
+    print(board[1] + '|' + board[2] + '|' + board[3])
+    print('-+-+-')
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('-+-+-')
+    print(board[7] + '|' + board[8] + '|' + board[9])
